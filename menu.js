fbc1c9c9499f9cfd678bd42eef9eb5af1f8763a8
FrogiiiNotFound <119341470+FrogiiiNotFound@users.noreply.github.com>
2025-10-14 18:58:31 +0300

    Delete menu.js

--- menu.js
+++
@@ -1,9 +0,0 @@
-const body = document.querySelector('body');
-const menuIcon = document.querySelector('.icon-menu');
-const menuBody = document.querySelector('.menu__body');
-
-menuIcon.addEventListener('click', (e) => {
-  menuIcon.classList.toggle('_active');
-  menuBody.classList.toggle('_active');
-  body.classList.toggle('_active');
-});
